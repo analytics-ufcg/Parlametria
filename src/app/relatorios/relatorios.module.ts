193f6697c6e23eb89098fc89a20a5f01cd53ead9
Jefferson Neves <jefferson.rpn@gmail.com>
2019-12-12 10:30:29 -0300

    Instala ng-bootstrap

--- src/app/relatorios/relatorios.module.ts
+++ src/app/relatorios/relatorios.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AgGridModule } from '@ag-grid-community/angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RelatoriosListarComponent } from './relatorios-listar/relatorios-listar.component';
 import { RelatoriosRoutingModule } from './relatorios-routing.module';
@@ -15,6 +16,7 @@ import { SociosEmpresasCampanhaComponent } from './socios-empresas-campanha/soci
   imports: [
     CommonModule,
     AgGridModule.withComponents([]),
+    NgbModule,
     RelatoriosRoutingModule
   ]
 })
